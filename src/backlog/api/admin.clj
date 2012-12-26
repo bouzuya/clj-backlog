8050967fa9a75cc89dee89f515b8ff8eef089798
bouzuya <bouzuya@gmail.com>
2012-12-27 01:39:35 +0900

    added backlog.admin.* method template. #close BAPI-74

---
+++ src/backlog/api/admin.clj
@@ -0,0 +1,77 @@
+(ns backlog.api.admin)
+
+; TODO: BAPI-46
+(defn get-users
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-47
+(defn add-user
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-48
+(defn update-user
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-49
+(defn delete-user
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-50
+(defn get-projects
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-51
+(defn add-project
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-52
+(defn update-project
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-53
+(defn delete-project
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-54
+(defn get-project-users
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-55
+(defn add-project-user
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-56
+(defn update-project-users
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-57
+(defn delete-project-user
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-58
+(defn add-custom-field
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-59
+(defn updadte-custom-field
+  []
+  (throw (UnsupportedOperationException.)))
+
+; TODO: BAPI-60
+(defn delete-custom-field
+  []
+  (throw (UnsupportedOperationException.)))
+
